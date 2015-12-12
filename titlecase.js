2839f87135c698cbbd6d74d6faf88472755ed68c
Mike Wood <MikeRWood@users.noreply.github.com>
2015-12-11 22:14:54 -0500

    FreeCodeCamp Bonfires - basic algorithm scripting

--- titlecase.js
+++ titlecase.js
@@ -16,4 +16,4 @@ titleCase("I'm a little tea pot");
 titleCase("sHoRt AnD sToUt"); 
 //Short And Stout
 titleCase("HERE IS MY HANDLE HERE IS MY SPOUT");
-//Here Is My Handle Here Is My Spout 
+//Here Is My Handle Here Is My Spout
