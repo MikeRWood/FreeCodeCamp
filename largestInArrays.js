850cb3e1b97733aa3da1f05bfa93d392e46e1269
Mike Wood <MikeRWood@users.noreply.github.com>
2015-12-13 19:33:08 -0500

    FreeCodeCamp Bonfires - basic algorithm scripting

--- largestInArrays.js
+++ largestInArrays.js
@@ -4,19 +4,16 @@
 +// Learn to Code at Free Code Camp (www.freecodecamp.com)
 
 function largestOfFour(arr) {
-  var results = [];
-  for (var n = 0; n < arr.length; n++) {
-    var largestNumber = 0;
-    for (var sb = 0; sb < arr[n].length; sb++) {
-      if (arr[n][sb] > largestNumber) {
-        largestNumber = arr[n][sb];
-      }
+  var largestArr = [];
+  for (var i=0; i < arr.length; i++) {
+    largestNum = 0;
+    for (var k=0; k < arr[i].length; k++) {
+      if(arr[i][k] > largestNum)
+        largestNum = arr[i][k];
+    largestArr[i] = largestNum;    
     }
-
-    results[n] = largestNumber;
   }
-
-  return results;
+  return largestArr;
 }
 
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
